8381de6b583190211e486f8f8da37c0abc9a654a
carlos <theycallhellp@gmail.com>
2022-06-08 11:23:19 -0700

    progress on index html and js and styles css

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an etch a sketch that will have boxes change colors when hovered over
---
+++ index.js
@@ -0,0 +1,12 @@
+const row = document.createElement("div");
+const square = document.createElement("div");
+
+row.className = "row";
+square.className = "square";
+
+document.getElementById("board").appendChild(row);
+document.getElementById("board").lastChild.appendChild(square);
+document.getElementById("board").lastChild.appendChild(square);
+// document.getElementById("board").lastChild.appendChild(square);
+// document.getElementById("board").lastChild.appendChild(square);
+// document.getElementById("board").lastChild.appendChild(square);
